275d19164f449642e428a8b8e100ecc7a2127e2d
unknown <amayadf@gmail.com>
2023-04-20 14:56:06 -0400

    Vivan los puntos de José y larga vida a Héctor, el rey de los tests

--- src/classes/Rectangle.py
+++ src/classes/Rectangle.py
@@ -1,7 +1,28 @@
 from src.classes.Point import Point
+import math
 
 
 class Rectangle:
     def __init__(self, point_one: Point, point_two: Point):
         self.point_one = point_one
         self.point_two = point_two
+
+    # método constructor para crear los puntos de la diagonal
+    # en caso de no recibir parámetros se crearán ambos puntos en el origen de coordenadas
+    @staticmethod
+    def builder(x1=0, y1=0, x2=0, y2=0):
+        return Rectangle(Point.builder(x1, y1), Point.builder(x2, y2))
+
+    # método que muestre la base del rectángulo
+    def base(self):
+        b = math.sqrt(math.pow(self.point_two.x - self.point_one.x, 2))
+        return b
+
+    # método que muestre la altura del rectángulo
+    def height(self):
+        h = math.sqrt(math.pow(self.point_two.y - self.point_one.y, 2))
+        return h
+
+    # método que muestre el área del rectángulo
+    def area(self):
+        return self.base() * self.height()
--- test/RectangleTest.py
+++ test/RectangleTest.py
@@ -4,16 +4,44 @@ import unittest
 # esto se hace para poder acceder a los módulos dentro de la carpeta src
 sys.path.append('./')
 
-from src.classes import Rectangle
+from src.classes import Rectangle, Point
+
 
 # clase para los tests correspondientes a la clase Rectangle
 class RectangleTest(unittest.TestCase):
-    # test de prueba
-    def test_example(self):
-        # eso verifica si la suma entre 5 y 5 espera que de como resultado 10
-        # si cumple pasará el test
-        # en caso contrario lanzará un error
-        self.assertEqual(5 + 5, 10)
+
+    def test_create_rectangle_with_parameters(self):
+        rectangle = Rectangle.builder(1, 2, 4, 4)
+
+        correct_point_one = rectangle.point_one == Point(1, 2)
+        correct_point_two = rectangle.point_two == Point(4, 4)
+
+        are_correct = bool(rectangle.point_one and rectangle.point_two)
+        self.assertTrue(are_correct)
+
+    def test_create_rectangle_no_parameters(self):
+        rectangle = Rectangle.builder()
+
+        correct_point_one = rectangle.point_one == Point(0, 0)
+        correct_point_two = rectangle.point_two == Point(0, 0)
+
+        are_correct = bool(rectangle.point_one and rectangle.point_two)
+        self.assertTrue(are_correct)
+
+    def test_base(self):
+        rectangle = Rectangle(Point(1, 1), Point(4, 3))
+        correct_base = 4 - 1
+        self.assertEqual(rectangle.base(), correct_base, "Debe ser 3")
+
+    def test_height(self):
+        rectangle = Rectangle(Point(1, 7), Point(4, 1))
+        correct_height = 7 - 1
+        self.assertEqual(rectangle.height(), correct_height, "Debe ser 6")
+
+    def test_area(self):
+        rectangle = Rectangle(Point(-1, 2), Point(3, 8))
+        correct_area = (3 - (-1)) * (8 - 2)
+        self.assertEqual(rectangle.area(), correct_area, "Debe ser 12")
 
 
 if __name__ == '__main__':
